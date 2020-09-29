21a6569f6889eb5edaa8d4f0c94fafe3983ce1e2
itzflowz <71831976+itzflowz@users.noreply.github.com>
2020-09-29 14:03:28 +0200

    Create croupier.py

---
+++ croupier.py
@@ -0,0 +1,32 @@
+import random
+class Croupier(Carte):
+
+  def __init__(self):
+    pass
+
+
+  def nouvelle_donne():
+    nd = Carte()
+
+  def melanger(self):
+    while len(paquet_m)!=52:#boucle pour mélanger
+      a=random.randint(0,51)
+      if paquet[a]!=0:
+        paquet_m.append(paquet[a])
+        paquet[a]=0
+
+    return paquet_m
+  
+  def couper(): #prend un indice au hasard
+    b=random.randint(0,51)
+
+
+
+
+  def distribuer():
+    
+
+
+
+
+

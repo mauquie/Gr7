04a2f81c0a2a45655c3a6d21ffd75d00db497e50
Tamachiii <71831074+Tamachiii@users.noreply.github.com>
2020-09-29 13:58:06 +0200

    Update poker1.py
    Classe Carte

--- poker1.py
+++ poker1.py
@@ -1,19 +1,16 @@
-
 class Carte:
 
-    def __init__(self):
-        pass
+  def __init__(self,paquet):
+    self.paquet=paquet
 
-    def initialisation(self): #Méthode: Création du paquet de carte
-        couleur=["P","C","K","T"]
-        rang=["2","3","4","5","6","7","8","9","10","V","D","R","A"]
-        paquet=[[] for i in range(52)]
-
-        compteur=0
-
-        for i in range (4):
-            for j in range (13):
-                paquet[compteur].append(rang[j])
-                paquet[compteur].append(couleur[i])
-                compteur=compteur+1
-        return paquet
+  def initialisation(self): #Méthode: Création du paquet de carte
+    couleur=["P","C","K","T"]
+    rang=["2","3","4","5","6","7","8","9","10","V","D","R","A"]
+    paquet=[[] for i in range(52)]
+    compteur=0
+    for i in range (4):
+      for j in range (13):
+        paquet[compteur].append(rang[j])
+        paquet[compteur].append(couleur[i])
+        compteur=compteur+1
+    self.paquet = paquet

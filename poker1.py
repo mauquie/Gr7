15301d91fe32f6d6bf413d8d81668310d0fb3d8a
Tamachiii <71831074+Tamachiii@users.noreply.github.com>
2020-10-01 15:45:27 +0200

    Update poker1.py
    Nouvelle classe Carte et Croupier

--- poker1.py
+++ poker1.py
@@ -1,55 +1,60 @@
+import random
+
 class Carte:
-
-  def __init__(self,paquet):
-    self.paquet=paquet
-
-  def initialisation(self): #Méthode: Création du paquet de carte
-    couleur=["P","C","K","T"]
-    rang=["2","3","4","5","6","7","8","9","10","V","D","R","A"]
-    paquet=[[] for i in range(52)]
-    compteur=0
-    
-    for i in range (4):
-      for j in range (13):
-        paquet[compteur].append(rang[j])
-        paquet[compteur].append(couleur[i])
-        compteur=compteur+1
-        
-    self.paquet = paquet
+    """
+    Constructeur qui initialisation les couleurs et les rangs des cartes
+    """
+    def __init__(self):
+        self.couleur=["P","C","K","T"]
+        self.rang=["2","3","4","5","6","7","8","9","10","V","D","R","A"]
 
 class Croupier(Carte):
 
-  def __init__(self):
-    pass
-
-  def nouvelle_donne():
-    nd = Carte()
-
-  def melanger():
-    paquet=[]
-    paquet_m=[]
-
-    while len(paquet_m)!=52:
-      a=random.randint(0,51)
-      if paquet[a]!=0:
-        paquet_m.append(paquet[a])
-        paquet[a]=0
+    def __init__(self):
+        Carte.__init__(self)    #Appel de la classe mère
+        self.paquet=[]  #Initialisation du paquet
         
-    return paquet
-  
-  def couper():
-    paquet=[]
-    l1=[]
-    l2=[]
-    a=random.randint(0,51)
-
-    for i in range(0,a):
-      l1.append(paquet[i])
-    for i in range (a,51):
-      l2.append(paquet[i])
-    paquet=l2+l1
+    def rassembler(self):   #Méthode: Créer le paquet
+        self.paquet=[[] for i in range(52)] #Créer les 52 places du paquet
+        compteur=0
+        for i in range (4):
+          for j in range (13):
+            self.paquet[compteur].append(self.rang[j])
+            self.paquet[compteur].append(self.couleur[i])
+            compteur=compteur+1
     
-    return paquet
+    def melanger(self): #Méthode: Mélanger le paquet
+        random.shuffle(self.paquet)
 
-  def distribuer():
-    pass
+    def couper(self):   #Méthode: Couper le paquet
+        liste_t1=[]     #Liste temporaire 1
+        liste_t2=[]     #Liste temporaire 2
+        a=random.randint(0,52)  #Valeur aléatoire
+        for i in range(0,a):
+          liste_t1.append(self.paquet[i])   #Ajouter de [0] à [a] les éléments de paquet à la liste temporaire 1
+        for i in range (a,52):
+          liste_t2.append(self.paquet[i])   #Ajouter de [a] à [52] les éléments de paquet à la liste temporaire 2
+
+        self.paquet=liste_t2+liste_t1       #Paquet est maintenant égal à liste temporaire 2 plus liste temporaire 1
+
+    def nouvelle_donne(self):   #Méthode: Créer un paquet en le mélangeant puis en le coupant
+        self.rassembler()
+        self.melanger()
+        self.couper()
+
+    def distribuer(self):   #Méthode: Distribuer au joueur
+        pass
+
+#Programme principal
+test = Croupier()
+print(len(test.paquet))
+print(test.paquet)
+test.rassembler()
+print(len(test.paquet))
+print("\n",test.paquet)
+test.melanger()
+print(len(test.paquet))
+print("\n",test.paquet)
+test.couper()
+print(len(test.paquet))
+print("\n",test.paquet)
